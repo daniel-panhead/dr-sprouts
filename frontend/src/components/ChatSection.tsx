df51e0b6b5d1913e548f1a83be278af15b54dece
Daniel Pan <57362494+daniel-panhead@users.noreply.github.com>
2024-03-02 16:49:44 -0800

    Some refactoring + logos

--- frontend/src/components/ChatSection.tsx
+++ frontend/src/components/ChatSection.tsx
@@ -5,30 +5,45 @@ const ChatSection = ({ initialQuery }: {initialQuery: string}) => {
   const [chatHistory, setChatHistory] = useState<ChatHistory>([]);
   const [currentQuery, setCurrentQuery] = useState("");
 
-  useEffect(() => {
+  const addMessageToHistory = (
+    newMessage: string,
+    userSent: boolean
+  ) => {
     setChatHistory((prev) => [
       ...prev,
       {
-        message: initialQuery,
+        message: newMessage,
         time: new Date(),
-        userSent: true
+        userSent
       }
     ])
+  }
+
+  useEffect(() => {
+    if (initialQuery != "") addMessageToHistory(initialQuery, true);
   }, [initialQuery]);
 
   const handleSendMessage = async () => {
     if (currentQuery != "") {
-      setChatHistory((prev) => [
-        ...prev,
-        {
-          message: currentQuery,
-          userSent: true,
-          time: new Date()
-        }
-      ])
+      addMessageToHistory(currentQuery, true);
+      const currentQueryCopy = (' ' + currentQuery).slice(1);
+      
       setCurrentQuery("");
+      await sendQuery(currentQueryCopy);
     }
+  }
 
+  const sendQuery = async (currentQuery: string) => {
+    const result = await fetch("http://127.0.0.1:8000", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({response: currentQuery})
+    })
+    const response = await result.text();
+    
+    addMessageToHistory(response, false);
   }
 
   return (
